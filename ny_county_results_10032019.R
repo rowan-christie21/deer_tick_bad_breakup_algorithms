0fd9564e8063ed098b0f0ec6fd83ac817cb92e4b
Sofie Margaret Christie (RIT Student) <smc9227@ad.rit.edu>
2019-10-09 00:34:02 -0400

    added rest of code for running the deer tick nymph density and pathogen presence in both life stages, and adjusted significance for all functions run to 0.05

--- ny_county_results_10032019.R
+++ ny_county_results_10032019.R
@@ -16,10 +16,10 @@
 library("ggplot2")
 
 #call to bad breakup algorithm developed by Dr. Christie Bahlai of Kent State
-source("D:/Ixodes_scapularis_research_2019/bad_breakup_tick_algorithms/christie_bahlai_bad_breakup/R_model/bad_breakup_script.R")
+source("D:/Ixodes_scapularis_research_2019/bad_breakup_tick_algorithms/bad_breakup_2/R_model/bad_breakup_script.R")
 
 #-------------------------------------------------#
-#Tick adults
+#Tick adult density
 
 #read in tick adult data
 tick_adults_data <- read.csv("D:/Ixodes_scapularis_research_2019/bad_breakup_tick_algorithms/data/Deer_Tick_Surveillance__Adults__Oct_to_Dec__excluding_Powassan_virus__Beginning_2008.csv", head = T)
@@ -29,16 +29,18 @@ counties <- unique(tick_adults_data$County)
 counties <- na.omit(counties)
 counties <- as.character(counties)
 
-#function_results <- data.frame(county=c(""), stability.time=c(""), Absolute.Range.Min.Value=c(""), Absolute.Range.Max.Value=c(""), Relative.Range.Min.Value=c(""), Relative.Range.Max.Value=c(""), stringsAsFactors=FALSE)
-#rbind(function_results,list("tree",43,3,12,43,55))
-
+#set up vectors to collect results of stability time, absolute range, relative range 
+#run on adult deer tick data for each county with at least 10 years data
 counties.with.10.years.data = c()
 stability.time = c()
 absolute.range.min.value = c()
 absolute.range.max.value = c()
 relative.range.min.value = c()
 relative.range.max.value = c()
+proportion.significant = c()
+proportion.wrong = c()
 
+sink(paste("ny_county_adult_tick_data_results",Sys.Date()))
 for (i in counties) {
   tryCatch({
     #location: i
@@ -49,35 +51,148 @@ for (i in counties) {
     tick_adults <- na.omit(tick_adults)
     #data now cleaned
 
-    #if number of years in location exceeds 9 run bad breakup algorithm
+    #if number of years in location exceeds 9 run functions
     if(nrow(tick_adults) >= 10) {
+      #i returns county name
       print(i)
       counties.with.10.years.data <- c(counties.with.10.years.data, i)
+      
+      #iterates through our targetted windows and returns summary statistics
       print("Multiple Breakups")
       print(multiple_breakups(tick_adults))
+      
+      #returns how many years it takes to reach stability
       print("Stability Time")
-      st <- stability_time(tick_adults, error_multiplyer = 0.5)
+      st <- stability_time(tick_adults)
       print(st)
       stability.time <- c(stability.time, st)
+      
+      #abs_range returns the absolute range of significant findings
       print("Absolute Range")
-      ar <- abs_range(tick_adults, only_significant = FALSE, significance = 0.5)
+      ar <- abs_range(tick_adults, only_significant = FALSE, significance = 0.05)
       print(ar)
       absolute.range.min.value <- c(absolute.range.min.value, ar[1])
       absolute.range.max.value <- c(absolute.range.max.value, ar[2])
+      
+      #relative_range returns the absolute over and under estimate compared to the slope of the longest series
       print("Relative Range")
       rr <- relative_range(tick_adults, only_significant = FALSE, significance = 0.05)
       print(rr)
       relative.range.min.value <- c(relative.range.min.value, rr[1])
       relative.range.max.value <- c(relative.range.max.value, rr[2])
       
+      #returns the proportion of total windows with statistically significant values
+      print("Proportion Significant")
+      ps <- proportion_significant(tick_adults, significance = 0.05)
+      print(ps)
+      proportion.significant <- c(proportion.significant, ps)
+      
+      #returns proportion of significant relationships that does not match the direction of the true slope
+      print("Proportion Significantly Wrong")
+      psw <- proportion_wrong(tick_adults, significance = 0.05)
+      print(psw)
+      proportion.wrong <- c(proportion.wrong, psw)
+      
+      #print pyramid plot to png
       png(filename = paste("D:/Ixodes_scapularis_research_2019/bad_breakup_tick_algorithms/Adult deer tick density in ", i, ", NY ",Sys.Date(),".png", sep = ''), width = 876, height = 604)
-      print(pyramid_plot(tick_adults, title=paste("Adult deer tick density in ", i, ", NY", sep = ''), plot_insig = TRUE, significance=0.1, rsq_points =TRUE, caption_plot = paste("Stability Time:", st)))
+      print(pyramid_plot(tick_adults, title=paste("Adult deer tick density in ", i, ", NY", sep = ''), plot_insig = TRUE, significance=0.05, rsq_points =TRUE, caption_plot = paste("Stability Time:", st)))
       dev.off()
 
     }
     
   }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
 }
+sink()
 
-county_adult_tick_data_results <- data.frame(counties.with.10.years.data, stability.time, absolute.range.min.value, absolute.range.max.value, relative.range.min.value, relative.range.max.value)
+county_adult_tick_data_results <- data.frame(counties.with.10.years.data, stability.time, absolute.range.min.value, absolute.range.max.value, relative.range.min.value, relative.range.max.value)#, proportion.significant, proportion.wrong)
 write.csv(county_adult_tick_data_results, file = paste("D:/Ixodes_scapularis_research_2019/bad_breakup_tick_algorithms/ny_county_adult_tick_data_results_",Sys.Date(),".csv",sep = ""))
+
+#-------------------------------------------------#
+#Tick adult pathogen presence
+
+#read in tick adult data
+tick_adults_data <- read.csv("D:/Ixodes_scapularis_research_2019/bad_breakup_tick_algorithms/data/Deer_Tick_Surveillance__Adults__Oct_to_Dec__excluding_Powassan_virus__Beginning_2008.csv", head = T)
+
+#create vector of counties in NY state in the dataset
+counties <- unique(tick_adults_data$County)
+counties <- na.omit(counties)
+counties <- as.character(counties)
+
+#set up vectors to collect results of stability time, absolute range, relative range 
+#run on adult deer tick data for each county with at least 10 years data
+counties.with.10.years.data = c()
+stability.time = c()
+absolute.range.min.value = c()
+absolute.range.max.value = c()
+relative.range.min.value = c()
+relative.range.max.value = c()
+proportion.significant = c()
+proportion.wrong = c()
+
+sink(paste("ny_county_adult_tick_pathogen_presence_data_results",Sys.Date()))
+for (i in counties) {
+  tryCatch({
+    #location: i
+    county <- tick_adults_data[tick_adults_data$County == i,]
+    #get only year and ticks found column
+    tick_pathogen <- county[,c("Year", "B..burgdorferi....")]
+    #omit nas
+    tick_pathogen <- na.omit(tick_pathogen)
+    #data now cleaned
+    
+    #if number of years in location exceeds 9 run functions
+    if(nrow(tick_pathogen) >= 10) {
+      #i returns county name
+      print(i)
+      counties.with.10.years.data <- c(counties.with.10.years.data, i)
+      
+      #iterates through our targetted windows and returns summary statistics
+      print("Multiple Breakups")
+      print(multiple_breakups(tick_pathogen))
+      
+      #returns how many years it takes to reach stability
+      print("Stability Time")
+      st <- stability_time(tick_pathogen)
+      print(st)
+      stability.time <- c(stability.time, st)
+      
+      #abs_range returns the absolute range of significant findings
+      print("Absolute Range")
+      ar <- abs_range(tick_pathogen, only_significant = FALSE, significance = 0.05)
+      print(ar)
+      absolute.range.min.value <- c(absolute.range.min.value, ar[1])
+      absolute.range.max.value <- c(absolute.range.max.value, ar[2])
+      
+      #relative_range returns the absolute over and under estimate compared to the slope of the longest series
+      print("Relative Range")
+      rr <- relative_range(tick_pathogen, only_significant = FALSE, significance = 0.05)
+      print(rr)
+      relative.range.min.value <- c(relative.range.min.value, rr[1])
+      relative.range.max.value <- c(relative.range.max.value, rr[2])
+      
+      #returns the proportion of total windows with statistically significant values
+      print("Proportion Significant")
+      ps <- proportion_significant(tick_pathogen, significance = 0.05)
+      print(ps)
+      proportion.significant <- c(proportion.significant, ps)
+      
+      #returns proportion of significant relationships that does not match the direction of the true slope
+      print("Proportion Significantly Wrong")
+      psw <- proportion_wrong(tick_pathogen, significance = 0.05)
+      print(psw)
+      proportion.wrong <- c(proportion.wrong, psw)
+      
+      #print pyramid plot to png
+      png(filename = paste("D:/Ixodes_scapularis_research_2019/bad_breakup_tick_algorithms/B Burgdorferi % in deer tick adults in ", i, ", NY ",Sys.Date(),".png", sep = ''), width = 876, height = 604)
+      print(pyramid_plot(tick_pathogen, title=paste("B. Burgdorferi % in deer tick adults in ", i, ", NY", sep = ''), plot_insig = TRUE, significance=0.05, rsq_points =TRUE, caption_plot = paste("Stability Time:", st)))
+      dev.off()
+      
+    }
+    
+  }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+}
+sink()
+
+county_adult_tick_data_results <- data.frame(counties.with.10.years.data, stability.time, absolute.range.min.value, absolute.range.max.value, relative.range.min.value, relative.range.max.value)#, proportion.significant, proportion.wrong)
+write.csv(county_adult_tick_data_results, file = paste("D:/Ixodes_scapularis_research_2019/bad_breakup_tick_algorithms/ny_county_adult_tick_data_results_",Sys.Date(),".csv",sep = ""))
+
